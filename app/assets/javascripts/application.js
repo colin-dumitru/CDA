c564afd0fb9cdaa3d96208209bee8ea172013016
Catalin Dumitru <colin.dumitru@gmail.com>
2013-12-16 21:01:27 +0200

    pending debt is now shown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,53 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+var timeElements = [];
+
+function start() {
+    $(".debt_container .timeLeft").each(function() {
+        timeElements.push({
+            elem: $(this),
+            start: new Date($(this).text())
+        });
+    });
+
+    updateTime();
+
+    setInterval(updateTime, 1000);
+}
+
+function updateTime() {
+    for (var i in timeElements) {
+        var elem = timeElements[i],
+            time = new Date(elem.start - Date.now()),
+            diff = dateDiff(Math.abs(time)),
+            sign = ' ';
+
+        if (time < 0) {
+            sign = '-';
+            elem.elem.css('color', 'red');
+        } else {
+            elem.elem.css('color', 'green');
+        }
+
+        elem.elem.text(sign + pad(diff.d, 5) + ':' + pad(diff.h,2,'0') + ':' + pad(diff.m,2,'0') + ':' + pad(diff.s,2,'0'));
+    }
+}
+
+function dateDiff( diff ) {
+    return {
+        ms : Math.floor( diff            % 1000 ),
+        s  : Math.floor( diff /     1000 %   60 ),
+        m  : Math.floor( diff /    60000 %   60 ),
+        h  : Math.floor( diff /  3600000 %   24 ),
+        d  : Math.floor( diff / 86400000        )
+    };
+}
+
+function pad(str, len, pad_str) {
+    str = String(str);
+
+    if (len <= str.length) {
+        return str;
+    } else {
+        return (pad_str || ' ') + pad(str, len - 1, pad_str);
+    }
+
+}
